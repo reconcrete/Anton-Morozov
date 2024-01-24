814790b58e80888a6965f4ae2b173b17fd4ad845
Anton Morozov <antonmorozoff64@gmail.com>
2024-01-24 13:28:56 +0100

    build

--- src/widgets/projectDescription/components/Projectdescription/Projectdescription.tsx
+++ src/widgets/projectDescription/components/Projectdescription/Projectdescription.tsx
@@ -11,7 +11,7 @@ export const ProjectDescription = () => {
       id="project-description"
       ref={projectDescriptionRef}
       className="ml-auto hidden min-h-[30%] w-[60%] flex-1 gap-[50px] pt-[70px] transition-all duration-300 ease-in-out lg:flex"
-    >
+    > 
       <h1 className="-translate-y-3 text-[34px] font-[600]">{currentProject.name}</h1>
       <p>{currentProject.longDescription}</p>{" "}
     </div>
