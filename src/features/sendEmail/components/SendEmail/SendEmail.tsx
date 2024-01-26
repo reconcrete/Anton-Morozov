628d204d88d668d68034853cf0cd59bbb2e1b1b3
Anton Morozov <antonmorozoff64@gmail.com>
2024-01-26 15:55:26 +0100

    update email

--- src/features/sendEmail/components/SendEmail/SendEmail.tsx
+++ src/features/sendEmail/components/SendEmail/SendEmail.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export const SendEmail = () => {
   return (
     <Link
-      href={"mailto:admorozov64@gmail.com"}
+      href={"mailto:ad.morozov64@gmail.com"}
       className="hidden items-center gap-x-2 rounded-[15px] bg-white px-5 py-2.5 text-black lg:flex"
     >
       <div className="h-[8px] w-[8px] animate-pulse rounded-full bg-[#A129FF]"></div>
